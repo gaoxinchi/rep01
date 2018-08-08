2ca3e1b939d0dd77a32da3993d0c4fbf3db5c113
gxc <gxc@DESKTOP-38JO738>
2018-08-08 19:55:46 +0800

    w1第一次提交

--- maven_git/src/main/java/com/gao/HelloWorld.java
+++ maven_git/src/main/java/com/gao/HelloWorld.java
@@ -3,4 +3,17 @@ package com.gao;
 public class HelloWorld {
 	private String name;
 	private String pass;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
 }
